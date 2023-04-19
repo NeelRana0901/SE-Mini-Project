739f4130460da075b93ab7dd591fa34b0bd995fb
Neel Rana <neelrana111@gmail.com>
2023-04-19 07:31:11 +0000

    final

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -6,12 +6,7 @@ const doctors = [
     Doctor_id: "004",
     Doctor_name: "Dr. Ashutosh Jokhakar",
     DocImage: "https://assets.lybrate.com/img/documents/doctor/dp/2bd34e5483e476fb6ed17e12a5fb8f31/General-Physician-SanjayGajera-Surat-7ec31f",
-    Treatment_id: "123",
-    Treatment_name: "NA",
     Diseases_name: "Malaria",
-    Treatment_cost: "NA",
-    Appointment_fee: "450",
-    Hospital_id: "48",
     Area: "Citylight",
     Hospital_name: "Suvidha Clinic",
     Hospital_full_address:
@@ -22,12 +17,7 @@ const doctors = [
     Doctor_id: "005",
     Doctor_name: "Dr. Sharat Gupta",
     DocImage:"https://content3.jdmagicbox.com/comp/surat/g8/0261px261.x261.170927134225.u3g8/catalogue/dr-sharat-gupta-city-light-skin-clinic-citylight-road-surat-dermatologists-ds2nd5lqtt.jpg",
-    Treatment_id: "124",
-    Treatment_name: "Chemotherapy",
     Diseases_name: "Cancer",
-    Treatment_cost: "NA",
-    Appointment_fee: "300",
-    Hospital_id: "49",
     Area: "Citylight",
     Hospital_name: "Healing Hand Hospital",
     Hospital_full_address:
@@ -37,12 +27,7 @@ const doctors = [
     Doctor_id: "008",
     Doctor_name: "Dr. Kaushik Soni",
     DocImage:"https://content3.jdmagicbox.com/comp/ahmedabad/44/079p81244/catalogue/dr-kaushik-vaidya-sola-road-ahmedabad-general-physician-doctors-nw3lu.jpg",
-    Treatment_id: "127",
-    Treatment_name: "NA",
     Diseases_name: "fever",
-    Treatment_cost: "NA",
-    Appointment_fee: "370",
-    Hospital_id: "52",
     Area: "Adajan",
     Hospital_name: "Nidan Clinic",
     Hospital_full_address: "Nidan Clinc,near Gujarat Gas station,Adajan Road",
@@ -51,12 +36,7 @@ const doctors = [
     Doctor_id: "006",
     Doctor_name: "Dr. Pradip Martin",
     DocImage:"https://media.licdn.com/dms/image/C4E03AQHqFV4FTvRYgw/profile-displayphoto-shrink_200_200/0/1517035668255?e=1681948800&v=beta&t=ITis7wN5WPwvxdcpWm7lE1J-fkVpN4-ognAopp_0S54",
-    Treatment_id: "125",
-    Treatment_name: "NA",
     Diseases_name: "Food Poisoning",
-    Treatment_cost: "NA",
-    Appointment_fee: "500",
-    Hospital_id: "50",
     Area: "Citylight",
     Hospital_name: "Healing Hand Hospital",
     Hospital_full_address:
@@ -66,12 +46,7 @@ const doctors = [
     Doctor_id: "011",
     Doctor_name: "Dr. Ramesh Patel",
     DocImage:"https://images.drlogy.com/assets/uploads/img/practice-profile/doctors/photo/dr-ramesh-n-patel-67f2fb1ee0e-b98ce30db01.jpg",
-    Treatment_id: "131",
-    Treatment_name: "NA",
     Diseases_name: "Back-Pain",
-    Treatment_cost: "NA",
-    Appointment_fee: "350",
-    Hospital_id: "54",
     Area: "Adajan",
     Hospital_name: "BAPS Pramukh Swami Hospital",
     Hospital_full_address:
@@ -82,12 +57,7 @@ const doctors = [
     Doctor_id: "000",
     Doctor_name: "Dr. Sanjay Agrawal",
     DocImage:"https://media.licdn.com/dms/image/C5603AQF51XNq2_tOkw/profile-displayphoto-shrink_800_800/0/1517553000076?e=2147483647&v=beta&t=Lkj-gJCJTdOMmvZUWC1c74KrFXH-wCfQwWiN7ICoq3M",
-    Treatment_id: "120",
     Diseases_name: "Diabetes",
-    Treatment_name: "NA",
-    Treatment_cost: "NA",
-    Appointment_fee: "300",
-    Hospital_id: "45",
     Area: "Citylight",
     Hospital_name: "Aman Hospital",
     Hospital_full_address:
@@ -97,12 +67,7 @@ const doctors = [
     Doctor_id: "002",
     Doctor_name: " Dr. Shailesh Gandhi",
     DocImage:"https://imagesx.practo.com/providers/dr-shailesh-gandhi-general-physician-surat-6c3d0acb-ed82-404e-a320-5f0106330bd3.jpg?i_type=t_100x100-4x",
-    Treatment_id: "121",
-    Treatment_name: "NA",
     Diseases_name: "Typhoid",
-    Treatment_cost: "NA",
-    Appointment_fee: "150",
-    Hospital_id: "46",
     Area: "Citylight",
     Hospital_name: "Shalibhadra Clinic",
     Hospital_full_address:
@@ -112,12 +77,7 @@ const doctors = [
     Doctor_id: "003",
     Doctor_name: "Dr. Yesha Chokshi",
     DocImage:"https://www.spiralshealth.com/public/userdata/doctor/TWxvNEdUcmhoakVpUEpleml3NEwyZz09/1614522685407648_spirals46290_1615627444.png",
-    Treatment_id: "122",
-    Treatment_name: "Delivery",
     Diseases_name: "WomanCare",
-    Treatment_cost: "NA",
-    Appointment_fee: "500",
-    Hospital_id: "47",
     Area: "Citylight",
     Hospital_name: "Shree Hospital",
     Hospital_full_address:
@@ -128,12 +88,7 @@ const doctors = [
     Doctor_id: "007",
     Doctor_name: "Dr. Jayesh Dubhuwala",
     DocImage:"https://media.licdn.com/dms/image/C5103AQGtchFP2Ui7Gg/profile-displayphoto-shrink_200_200/0/1516976699160?e=1684972800&v=beta&t=8mEr_yWVRcseEIe5C6VijEok3pE91xtvn1FiX_YZqfM",
-    Treatment_id: "126",
-    Treatment_name: "NA",
     Diseases_name: "Skin Related",
-    Treatment_cost: "NA",
-    Appointment_fee: "350",
-    Hospital_id: "51",
     Area: "Citylight",
     Hospital_name: "City light Skin Clinic",
     Hospital_full_address:
@@ -197,21 +152,13 @@ const DoctorFinder = () => {
             <p>No Doctor Found</p>
           ) : (
             <>
-              {/* <p>{getDoctorSuggestions().length} doctors found:</p> */}
-              {/* <br /> */}
               <ul>
                 {getDoctorSuggestions().map((doctor) => (
                   <ul className="onerow" key={doctor.Doctor_id} type="disc">
                     <img className="doctorsimage" src={doctor.DocImage} />
                     <p className="docText">Doctor Name <span className="docSpec">: {doctor.Doctor_name}</span> </p>
                     <p className="docText">Treatment Specialist <span className="docSpec">: {doctor.Diseases_name} Specialist</span> </p>
-                    <p className="docText">Full Address <span className="docSpec">: {doctor.Hospital_full_address}</span></p> 
-                    {/* <br /> */}
-                    {/* <b>Treatment Name</b> : {doctor.Treatment_name}{" "} */}
-                    {/* <b>Hospital Name</b> : {doctor.Hospital_name} */}
-                    {/* <br /> */}
-                    {/* <b>Appointment Fees</b> : {doctor.Appointment_fee}{" "} */}
-                    {/* <br />*/}
+                    <p className="docText">Full Address <span className="docSpec">: {doctor.Hospital_full_address}</span></p>
                   </ul>
                 ))}
               </ul>
